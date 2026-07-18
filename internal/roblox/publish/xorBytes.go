1a0674d65030b67adff37312cf56522aeef9ffca
Timspects <109029750+Timspects@users.noreply.github.com>
2026-07-18 17:46:30 +0300

    Implement xorBytes function for byte manipulation

--- internal/roblox/publish/xorBytes.go
+++ internal/roblox/publish/xorBytes.go
@@ -1,1 +1,20 @@
 package publish
+
+import (
+	"bytes"
+	"encoding/base64"
+	"encoding/json"
+	"errors"
+	"io"
+	"net/http"
+
+	"github.com/Timspects/roblox-kartfr-asset-reuploader-modified-by-timspects/internal/roblox"
+)
+
+func xorBytes(data []byte, key byte) []byte {
+	out := make([]byte, len(data))
+	for i := range data {
+		out[i] = data[i] ^ key
+	}
+	return out
+}
