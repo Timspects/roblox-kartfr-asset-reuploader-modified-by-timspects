9b80c72000e0c916959cf09d61f67fc25c01349b
Timspects <109029750+Timspects@users.noreply.github.com>
2026-07-18 17:51:51 +0300

    Update xorBytes.go

--- internal/roblox/publish/xorBytes.go
+++ internal/roblox/publish/xorBytes.go
@@ -1,16 +1,5 @@
 package publish
 
-import (
-	"bytes"
-	"encoding/base64"
-	"encoding/json"
-	"errors"
-	"io"
-	"net/http"
-
-	"github.com/Timspects/roblox-kartfr-asset-reuploader-modified-by-timspects/internal/roblox"
-)
-
 func xorBytes(data []byte, key byte) []byte {
 	out := make([]byte, len(data))
 	for i := range data {
